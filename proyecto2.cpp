c4a882a61b04bb9482df12b4d3129276caead755
kathemantunez <kathe_mantunez@unitec.edu>
2018-05-04 16:01:14 +0300

    ejercicio2

--- proyecto2.cpp
+++ proyecto2.cpp
@@ -7,7 +7,9 @@ using namespace std;
 //metodos
 
 void ejercicio2();
-void metodo_circulos(int.int,int,int);
+void ejercicio1();
+void ejercicio3();
+void metodo_circulos(int,int,int,int,int,int,int,int);
 
 
 //--------------
@@ -35,7 +37,7 @@ int main()
 			case '1':
 			
 				cout<<"Elegiste el ejercicio 1.\n";
-	
+				ejercicio1();
 				break;
  
 			case '2':
@@ -47,7 +49,8 @@ int main()
 			case '3':
 				
 				cout << "Elegiste el ejercicio 3.\n";
-		
+				ejercicio3();
+
 				break;
  
 			case '4':
@@ -73,13 +76,15 @@ int main()
 void ejercicio2(){
 	bool bandera=false;
 	char tecla;
-	int x1=0,x2=0,y1=0,y2=0;
+	int x1=0,x2=0,y1=0,y2=0,w1=0,w2=0,z1=0,z2=0;
 
     	cout<<"\t1.ingresar numeros"<<endl;
 	cout<<"\t2.numeros random"<<endl;
 	cin>>tecla;
 	switch(tecla){
-		case '1': cout<<"ingrese x1:"<<endl;
+		case '1':
+			cout<<"primer circulo"<<endl;
+		       	cout<<"ingrese x1:"<<endl;
 			  cin>>x1;
 			  cout<<"ingrese x2:"<<endl;
 			  cin>>x2;
@@ -88,34 +93,50 @@ void ejercicio2(){
 			  cout<<"ingrese y2:"<<endl;
 			  cin>>y2;
 			  
-			void metodo_circulos(x1,x2,y1,y2);
+
+			  cout <<"segundo circulo"<<endl;
+			  cout<<"ingrese w1"<<endl;
+			  cin>>w1;
+			  cout<<"ingrese w2:"<<endl;
+			  cin>>w2;
+			  cout<<"ingrese z1:"<<endl;
+			  cin>>z1;
+			  cout<< "ingrese z2"<<endl;
+			  cin>>z2;
+
+
+			metodo_circulos(x1,x2,y1,y2,w1,w2,z1,z2);
+			
+			
 
 
 			  break;
 		case '2': 
 			  int c;
-			  int cont=1;
+			  int e;
+			  int cont=0;
+			  int cont2=0;
 
 			  srand(time(NULL));
 			
 			  for(c=1;c<=10;c++){
-				while(cont<=4){
-				if(cont==1){
+				while(cont<4){
+				if(cont==0){
 				x1=1+rand()%(11-1);
 			//	cout<<x1<<endl;
 				cont++;
 				}
-				if(cont==2){
+				if(cont==1){
 				x2=1+rand()%(11-1);
 			//	cout<<x2<<endl;
 				cont++;
 				}
-				if(cont==3){
+				if(cont==2){
 				y1=1+rand()%(11-1);
 			//	cout<<y1<<endl;
 				cont++;
 				}
-				if(cont ==4){
+				if(cont ==3){
 				y2=1+rand()%(11-1);
 			//	cout<<y2<<endl;
 				cont++;
@@ -124,20 +145,53 @@ void ejercicio2(){
 
 
 			  }
-			void metodo_circulos(x1,x2,y1,y2);	
+
+
+			  for(e=1;e<=4;e++){
+				if(e==1){
+				w1=1+rand()%(11-1);
+
+				}
+				if(e==2){
+				w2=1+rand()%(11-1);
+				}
+				if(e==3){
+				z1=1+rand()%(11-1);
+				}
+				if(e==4){
+				z2=1+rand()%(11-1);
+				}
+
+			  }
+		  metodo_circulos(x1,x2,y1,y2,w1,w2,z1,z2);
+				
 
 			  break;
-		default:cout<<"opciòn incorrecta"<<endl;
-
-			break;
+		
 
 	}
 }
+void  metodo_circulos(int x1,int x2,int y1,int y2,int w1,int w2,int z1,int z2){
+	int distancia=0;
+	int distancia2=0;
+	int x,y;
+	int k,m;
+	//
+	x=pow((x2-x1),2);
+	y=pow((y2-y1),2);
+	distancia=sqrt(x+y);
 
-void metodo_circulos(int x1,int x2,int y1,int y2){
+	k=pow((w2-w1),2);
+	m=pow((z2-z1),2);
+	distancia2=sqrt(k+m);
+
+	cout<<"distancia circulo 1:"+distancia<<endl;
+	cout<<"distancia circulo 2:"+distancia2<<endl;	
 
 
 }
+void ejercicio1(){
 
+}
 
 
