35270de370dbd6421cf5216bf43386dd28a66b49
kathemantunez <kathe_mantunez@unitec.edu>
2018-05-04 14:42:57 +0300

    metodos ,ejercicio1

--- proyecto2.cpp
+++ proyecto2.cpp
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <math.h>
 using namespace std;
- 
+
+//metodos
+
+void ejercicio2();
+void metodo_circulos(int.int,int,int);
+
+
+//--------------
 int main()
 {
     bool bandera=false;
@@ -34,7 +41,7 @@ int main()
 			case '2':
 			
 				cout << "Elegiste el ejercicio 2.\n";
-	
+				ejercicio2();
 				break;
  
 			case '3':
@@ -61,6 +68,76 @@ int main()
 
 }
 
-//ejercicio 1
+//ejercicio 2
+//
+void ejercicio2(){
+	bool bandera=false;
+	char tecla;
+	int x1=0,x2=0,y1=0,y2=0;
+
+    	cout<<"\t1.ingresar numeros"<<endl;
+	cout<<"\t2.numeros random"<<endl;
+	cin>>tecla;
+	switch(tecla){
+		case '1': cout<<"ingrese x1:"<<endl;
+			  cin>>x1;
+			  cout<<"ingrese x2:"<<endl;
+			  cin>>x2;
+			  cout<<"ingrese y1:"<<endl;
+			  cin>>y1;
+			  cout<<"ingrese y2:"<<endl;
+			  cin>>y2;
+			  
+			void metodo_circulos(x1,x2,y1,y2);
+
+
+			  break;
+		case '2': 
+			  int c;
+			  int cont=1;
+
+			  srand(time(NULL));
+			
+			  for(c=1;c<=10;c++){
+				while(cont<=4){
+				if(cont==1){
+				x1=1+rand()%(11-1);
+			//	cout<<x1<<endl;
+				cont++;
+				}
+				if(cont==2){
+				x2=1+rand()%(11-1);
+			//	cout<<x2<<endl;
+				cont++;
+				}
+				if(cont==3){
+				y1=1+rand()%(11-1);
+			//	cout<<y1<<endl;
+				cont++;
+				}
+				if(cont ==4){
+				y2=1+rand()%(11-1);
+			//	cout<<y2<<endl;
+				cont++;
+				}
+				}
+
+
+			  }
+			void metodo_circulos(x1,x2,y1,y2);	
+
+			  break;
+		default:cout<<"opciòn incorrecta"<<endl;
+
+			break;
+
+	}
+}
+
+void metodo_circulos(int x1,int x2,int y1,int y2){
+
+
+}
+
 
 
